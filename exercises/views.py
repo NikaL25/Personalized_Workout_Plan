7d7f5c091b5f4e05da0e99e85d0a76a2dfce9ea1
nika-lomiashvili <mr.lomiashvili@mail.ru>
2024-03-14 04:08:48 +0400

    started new Django Project: Personalized_Workout_Plan

--- exercises/views.py
+++
@@ -1,9 +0,0 @@
-# In your_app/views.py
-
-from rest_framework import viewsets
-from .models import Exercise
-from .serializers import ExerciseSerializer
-
-class ExerciseViewSet(viewsets.ModelViewSet):
-    queryset = Exercise.objects.all()
-    serializer_class = ExerciseSerializer
--- workouts/admin.py
+++ workouts/admin.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-from .models import WorkoutPlan, WeightTracking, FitnessGoal, CompletedExercise
-# Register your models here.
+# from django.contrib import admin
+# from .models import WorkoutPlan, WeightTracking, FitnessGoal, CompletedExercise
+# # Register your models here.
 
-admin.site.register(WorkoutPlan)
-admin.site.register(WeightTracking)
-admin.site.register(FitnessGoal)
-admin.site.register(CompletedExercise)
+# admin.site.register(WorkoutPlan)
+# admin.site.register(WeightTracking)
+# admin.site.register(FitnessGoal)
+# admin.site.register(CompletedExercise)
